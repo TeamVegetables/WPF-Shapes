da6a344d36e01199e2b287475bb695ebd78c61c2
Ivan Frejuk <frejukivan@gmail.com>
2018-10-12 22:25:45 +0300

    Added pentagon dragging

--- WPF-Shapes/BLL/Commands/DrawPentagonCommand.cs
+++ WPF-Shapes/BLL/Commands/DrawPentagonCommand.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -20,6 +21,10 @@ namespace WPF_Shapes.BLL.Commands
 
         private int _count;
 
+        private Point? dragStart = null;
+
+        private Canvas _canvas;
+
         public DrawPentagonCommand(DataContext dataContext)
         {
             _pointCollection = new PointCollection();
@@ -34,8 +39,8 @@ namespace WPF_Shapes.BLL.Commands
         public void Execute(object parameter)
         {
             var grid = parameter as Grid;
-            var canvas = (Canvas)((Border)((ScrollViewer)grid?.Children[6])?.Content)?.Child;
-            _pointCollection.Add(Mouse.GetPosition(canvas));
+            _canvas = (Canvas)((Border)((ScrollViewer)grid?.Children[6])?.Content)?.Child;
+            _pointCollection.Add(Mouse.GetPosition(_canvas));
             ++_count;
             if (_count == 5)
             {
@@ -48,11 +53,44 @@ namespace WPF_Shapes.BLL.Commands
                     StrokeThickness = _dataContext.Settings.StrokeThickness,
                     Fill = _dataContext.Settings.FillColorBrush
                 };
-                canvas?.Children.Add(polygon);
+                EnableDrag(polygon);
+                _canvas?.Children.Add(polygon);
                 _pointCollection.Clear();
                 _count = 0;
                 _dataContext.Manager.Add(polygon, name);
                 listBox?.Items.Refresh();
+            }
+        }
+
+        private void EnableDrag(UIElement element)
+        {
+            element.MouseDown += OnMouseDown;
+            element.MouseMove += OnMouseMove;
+            element.MouseUp += OnMouseUp;
+        }
+
+        private void OnMouseDown(object sender, MouseButtonEventArgs args)
+        {
+            var element = (UIElement)sender;
+            dragStart = args.GetPosition(element);
+            element.CaptureMouse();
+        }
+
+        private void OnMouseUp(object sender, MouseButtonEventArgs args)
+        {
+            var element = (UIElement)sender;
+            dragStart = null;
+            element.ReleaseMouseCapture();
+        }
+
+        private void OnMouseMove(object sender, MouseEventArgs args)
+        {
+            if (dragStart != null && args.LeftButton == MouseButtonState.Pressed)
+            {
+                var element = (UIElement)sender;
+                var p2 = args.GetPosition(_canvas);
+                Canvas.SetLeft(element, p2.X - dragStart.Value.X);
+                Canvas.SetTop(element, p2.Y - dragStart.Value.Y);
             }
         }
     }
